cf05da825cc67dbc45cd6fbb99d466a1c6c3750e
Tee Parham <tee@neighborland.com>
2017-11-27 15:37:30 -0700

    Fix tests, allow rc version

--- test/proj4_test.rb
+++ test/proj4_test.rb
@@ -2,6 +2,6 @@ require "test_helper"
 
 class Proj4Test < Test::Unit::TestCase
   def test_gem_version
-    assert_match(/^\d+\.\d+(\.\d+)?$/, ::RGeo::Proj4::VERSION)
+    assert_match(/^\d+\.\d+(\.\d+)(\.rc\d+)?$/, ::RGeo::Proj4::VERSION)
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 require "test/unit"
 require "rgeo/proj4"
 require "common/factory_tests"
+require "psych"
 
 require "pry-byebug" unless ENV["TRAVIS"]
