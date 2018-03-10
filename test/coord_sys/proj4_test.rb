265cf907e54b075934743cfa87678378f04aa713
Tee Parham <tee@neighborland.com>
2018-03-10 10:42:17 -0700

    Remove trailing _ from local variable names

--- test/coord_sys/proj4_test.rb
+++ test/coord_sys/proj4_test.rb
@@ -7,126 +7,126 @@ class TestProj4 < Test::Unit::TestCase # :nodoc:
   end
 
   def test_create_wgs84
-    proj_ = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-    assert_equal(true, proj_.geographic?)
-    assert_equal("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs", proj_.original_str)
-    assert_equal(" +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0", proj_.canonical_str)
+    proj = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    assert_equal(true, proj.geographic?)
+    assert_equal("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs", proj.original_str)
+    assert_equal(" +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0", proj.canonical_str)
   end
 
   def test_get_wgs84_geographic
-    proj_ = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-    proj2_ = proj_.get_geographic
-    assert_nil(proj2_.original_str)
-    assert_equal(true, proj2_.geographic?)
-    coords_ = RGeo::CoordSys::Proj4.transform_coords(proj_, proj2_, 1, 2, 0)
-    assert_equal([1, 2, 0], coords_)
+    proj = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    proj2 = proj.get_geographic
+    assert_nil(proj2.original_str)
+    assert_equal(true, proj2.geographic?)
+    coords = RGeo::CoordSys::Proj4.transform_coords(proj, proj2, 1, 2, 0)
+    assert_equal([1, 2, 0], coords)
   end
 
   def test_identity_transform
-    proj_ = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-    assert_equal([1, 2, 0], RGeo::CoordSys::Proj4.transform_coords(proj_, proj_, 1, 2, 0))
-    assert_equal([1, 2], RGeo::CoordSys::Proj4.transform_coords(proj_, proj_, 1, 2, nil))
+    proj = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    assert_equal([1, 2, 0], RGeo::CoordSys::Proj4.transform_coords(proj, proj, 1, 2, 0))
+    assert_equal([1, 2], RGeo::CoordSys::Proj4.transform_coords(proj, proj, 1, 2, nil))
   end
 
   def test_simple_mercator_transform
-    geography_ = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs", radians: true)
-    projection_ = RGeo::CoordSys::Proj4.create("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs")
-    assert_xy_close(project_merc(0, 0), RGeo::CoordSys::Proj4.transform_coords(geography_, projection_, 0, 0, nil))
-    assert_xy_close(project_merc(0.01, 0.01), RGeo::CoordSys::Proj4.transform_coords(geography_, projection_, 0.01, 0.01, nil))
-    assert_xy_close(project_merc(1, 1), RGeo::CoordSys::Proj4.transform_coords(geography_, projection_, 1, 1, nil))
-    assert_xy_close(project_merc(-1, -1), RGeo::CoordSys::Proj4.transform_coords(geography_, projection_, -1, -1, nil))
-    assert_xy_close(unproject_merc(0, 0), RGeo::CoordSys::Proj4.transform_coords(projection_, geography_, 0, 0, nil))
-    assert_xy_close(unproject_merc(10_000, 10_000), RGeo::CoordSys::Proj4.transform_coords(projection_, geography_, 10_000, 10_000, nil))
-    assert_xy_close(unproject_merc(-20_000_000, -20_000_000), RGeo::CoordSys::Proj4.transform_coords(projection_, geography_, -20_000_000, -20_000_000, nil))
+    geography = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs", radians: true)
+    projection = RGeo::CoordSys::Proj4.create("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs")
+    assert_xy_close(project_merc(0, 0), RGeo::CoordSys::Proj4.transform_coords(geography, projection, 0, 0, nil))
+    assert_xy_close(project_merc(0.01, 0.01), RGeo::CoordSys::Proj4.transform_coords(geography, projection, 0.01, 0.01, nil))
+    assert_xy_close(project_merc(1, 1), RGeo::CoordSys::Proj4.transform_coords(geography, projection, 1, 1, nil))
+    assert_xy_close(project_merc(-1, -1), RGeo::CoordSys::Proj4.transform_coords(geography, projection, -1, -1, nil))
+    assert_xy_close(unproject_merc(0, 0), RGeo::CoordSys::Proj4.transform_coords(projection, geography, 0, 0, nil))
+    assert_xy_close(unproject_merc(10_000, 10_000), RGeo::CoordSys::Proj4.transform_coords(projection, geography, 10_000, 10_000, nil))
+    assert_xy_close(unproject_merc(-20_000_000, -20_000_000), RGeo::CoordSys::Proj4.transform_coords(projection, geography, -20_000_000, -20_000_000, nil))
   end
 
   def test_equivalence
-    proj1_ = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-    proj2_ = RGeo::CoordSys::Proj4.create(" +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-    assert_equal(proj1_, proj2_)
+    proj1 = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    proj2 = RGeo::CoordSys::Proj4.create(" +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    assert_equal(proj1, proj2)
   end
 
   def test_hashes_equal_for_equivalent_objects
-    proj1_ = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-    proj2_ = RGeo::CoordSys::Proj4.create(" +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-    assert_equal(proj1_.hash, proj2_.hash)
+    proj1 = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    proj2 = RGeo::CoordSys::Proj4.create(" +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    assert_equal(proj1.hash, proj2.hash)
   end
 
   def test_point_projection_cast
-    geography_ = RGeo::Geos.factory(proj4: "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs", srid: 4326)
-    projection_ = RGeo::Geos.factory(proj4: "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs", srid: 27_700)
-    proj_point_ = projection_.parse_wkt("POINT(473600.5000000000000000 186659.7999999999883585)")
-    geo_point_ = RGeo::Feature.cast(proj_point_, project: true, factory: geography_)
-    assert_close_enough(-0.9393598527244420, geo_point_.x)
-    assert_close_enough(51.5740106527552697, geo_point_.y)
+    geography = RGeo::Geos.factory(proj4: "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs", srid: 4326)
+    projection = RGeo::Geos.factory(proj4: "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs", srid: 27_700)
+    proj_point = projection.parse_wkt("POINT(473600.5000000000000000 186659.7999999999883585)")
+    geo_point = RGeo::Feature.cast(proj_point, project: true, factory: geography)
+    assert_close_enough(-0.9393598527244420, geo_point.x)
+    assert_close_enough(51.5740106527552697, geo_point.y)
   end
 
   def test_point_transform_lowlevel
-    geography_ = RGeo::Geos.factory(proj4: "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs", srid: 4326)
-    projection_ = RGeo::Geos.factory(proj4: "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs", srid: 27_700)
-    proj_point_ = projection_.parse_wkt("POINT(473600.5000000000000000 186659.7999999999883585)")
-    geo_point_ = RGeo::CoordSys::Proj4.transform(projection_.proj4, proj_point_, geography_.proj4, geography_)
-    assert_close_enough(-0.9393598527244420, geo_point_.x)
-    assert_close_enough(51.5740106527552697, geo_point_.y)
+    geography = RGeo::Geos.factory(proj4: "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs", srid: 4326)
+    projection = RGeo::Geos.factory(proj4: "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs", srid: 27_700)
+    proj_point = projection.parse_wkt("POINT(473600.5000000000000000 186659.7999999999883585)")
+    geo_point = RGeo::CoordSys::Proj4.transform(projection.proj4, proj_point, geography.proj4, geography)
+    assert_close_enough(-0.9393598527244420, geo_point.x)
+    assert_close_enough(51.5740106527552697, geo_point.y)
   end
 
   def test_geocentric
-    obj1_ = RGeo::CoordSys::Proj4.create("+proj=geocent +ellps=WGS84")
-    assert_equal(true, obj1_.geocentric?)
+    obj1 = RGeo::CoordSys::Proj4.create("+proj=geocent +ellps=WGS84")
+    assert_equal(true, obj1.geocentric?)
   end
 
   def test_get_geographic
-    projection_ = RGeo::CoordSys::Proj4.create("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs")
-    geographic_ = projection_.get_geographic
-    expected_ = RGeo::CoordSys::Proj4.create("+proj=latlong +a=6378137 +b=6378137 +nadgrids=@null")
-    assert_equal(expected_, geographic_)
+    projection = RGeo::CoordSys::Proj4.create("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs")
+    geographic = projection.get_geographic
+    expected = RGeo::CoordSys::Proj4.create("+proj=latlong +a=6378137 +b=6378137 +nadgrids=@null")
+    assert_equal(expected, geographic)
   end
 
   def test_marshal_roundtrip
-    obj1_ = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-    dump_ = ::Marshal.dump(obj1_)
-    obj2_ = ::Marshal.load(dump_)
-    assert_equal(obj1_, obj2_)
+    obj1 = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    dump = ::Marshal.dump(obj1)
+    obj2 = ::Marshal.load(dump)
+    assert_equal(obj1, obj2)
   end
 
   def test_dup
-    obj1_ = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-    obj2_ = obj1_.dup
-    assert_equal(obj1_, obj2_)
+    obj1 = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    obj2 = obj1.dup
+    assert_equal(obj1, obj2)
   end
 
   def test_dup_of_get_geographic
-    obj1_ = RGeo::CoordSys::Proj4.create("+proj=latlong +datum=WGS84 +ellps=WGS84")
-    obj2_ = obj1_.get_geographic
-    obj3_ = obj2_.dup
-    assert_equal(obj1_, obj3_)
+    obj1 = RGeo::CoordSys::Proj4.create("+proj=latlong +datum=WGS84 +ellps=WGS84")
+    obj2 = obj1.get_geographic
+    obj3 = obj2.dup
+    assert_equal(obj1, obj3)
   end
 
   def test_yaml_roundtrip
-    obj1_ = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-    dump_ = Psych.dump(obj1_)
-    obj2_ = Psych.load(dump_)
-    assert_equal(obj1_, obj2_)
+    obj1 = RGeo::CoordSys::Proj4.create("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    dump = Psych.dump(obj1)
+    obj2 = Psych.load(dump)
+    assert_equal(obj1, obj2)
   end
 
   private
 
-  def project_merc(x_, y_)
-    [x_ * 6_378_137.0, ::Math.log(::Math.tan(::Math::PI / 4.0 + y_ / 2.0)) * 6_378_137.0]
+  def project_merc(x, y)
+    [x * 6_378_137.0, ::Math.log(::Math.tan(::Math::PI / 4.0 + y / 2.0)) * 6_378_137.0]
   end
 
-  def unproject_merc(x_, y_)
-    [x_ / 6_378_137.0, (2.0 * ::Math.atan(::Math.exp(y_ / 6_378_137.0)) - ::Math::PI / 2.0)]
+  def unproject_merc(x, y)
+    [x / 6_378_137.0, (2.0 * ::Math.atan(::Math.exp(y / 6_378_137.0)) - ::Math::PI / 2.0)]
   end
 
-  def assert_close_enough(a_, b_)
-    delta_ = ::Math.sqrt(a_ * a_ + b_ * b_) * 0.00000001
-    delta_ = 0.000000000001 if delta_ < 0.000000000001
-    assert_in_delta(a_, b_, delta_)
+  def assert_close_enough(a, b)
+    delta = ::Math.sqrt(a * a + b * b) * 0.00000001
+    delta = 0.000000000001 if delta < 0.000000000001
+    assert_in_delta(a, b, delta)
   end
 
-  def assert_xy_close(xy1_, xy2_)
-    assert_close_enough(xy1_[0], xy2_[0])
-    assert_close_enough(xy1_[1], xy2_[1])
+  def assert_xy_close(xy1, xy2)
+    assert_close_enough(xy1[0], xy2[0])
+    assert_close_enough(xy1[1], xy2[1])
   end
 end
