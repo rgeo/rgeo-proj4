438e962b2789360b8f0dc5cd760cca59254fd036
Tee Parham <tee@neighborland.com>
2018-12-08 15:12:43 -0700

    Cleanup test

--- test/projected_geographic/multi_point_test.rb
+++ test/projected_geographic/multi_point_test.rb
@@ -6,9 +6,11 @@ require "common/multi_point_tests"
 class TestMultiPoint < Minitest::Test # :nodoc:
   include RGeo::Tests::Common::MultiPointTests
 
-  def create_factory(opts_ = {})
-    RGeo::Geographic.projected_factory(opts_.merge(
-      projection_proj4: "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs", projection_srid: 3857))
+  def create_factory(opts = {})
+    RGeo::Geographic.projected_factory opts.merge \
+      projection_proj4: "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 "\
+                        "+x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs",
+      projection_srid: 3857
   end
 
   # These tests suffer from floating point issues
