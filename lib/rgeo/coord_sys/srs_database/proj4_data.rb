3ecb670c6a9aec148b28f42f73d01613c1c077fd
Keith Doggett <keith.doggett887@gmail.com>
2021-02-22 18:51:28 -0500

    Use github actions

--- lib/rgeo/coord_sys/srs_database/proj4_data.rb
+++ lib/rgeo/coord_sys/srs_database/proj4_data.rb
@@ -81,11 +81,12 @@ module RGeo
           ident_ = ident_.to_s
           return @cache[ident_] if @cache&.include?(ident_)
           result_ = nil
-          if @populate_state == 0
+          case @populate_state
+          when 0
             data_ = search_file(ident_)
             result_ = Entry.new(ident_, authority: @authority, authority_code: @authority ? ident_ : nil, name: data_[1], proj4: data_[2]) if data_
             @cache[ident_] = result_ if @cache
-          elsif @populate_state == 1
+          when 1
             search_file(nil)
             result_ = @cache[ident_]
             @populate_state = 2
--- test/coord_sys/proj4_srs_data_test.rb
+++ test/coord_sys/proj4_srs_data_test.rb
@@ -3,7 +3,7 @@
 require "test_helper"
 
 class TestProj4SRSData < Minitest::Test # :nodoc:
-  def test_nad83_4601
+  def test_nad83_4601 # rubocop:disable Naming/VariableNumber
     db_ = RGeo::CoordSys::SRSDatabase::Proj4Data.new("nad83")
     entry_ = db_.get(4601)
     assert_equal("proj=lcc  datum=NAD83 lon_0=-120d50 lat_1=48d44 lat_2=47d30 lat_0=47 x_0=500000 y_0=0 no_defs", entry_.proj4.original_str)
