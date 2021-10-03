56f04f2bf7caeca75ddb7c51fe200e8f0c72eadb
Keith Doggett <keith.doggett887@gmail.com>
2021-10-03 13:38:05 -0400

    Fix `Psych.load` Issue (#21)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,13 @@ require "rgeo/proj4"
 require "common/factory_tests"
 require "psych"
 require "pry-byebug"
+
+# Only here for Psych 4.0.0 breaking change.
+# See https://github.com/ruby/psych/pull/487
+def psych_load(*args)
+  if Psych.respond_to?(:unsafe_load)
+    Psych.unsafe_load(*args)
+  else
+    Psych.load(*args)
+  end
+end
